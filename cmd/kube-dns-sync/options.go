044bcd26107bd021f1b8a927d7c11fc9f0b21bdd
Chi Vinh Le <vinh@wikiwi.io>
2016-07-22 17:31:41 +0700

    Rename environment variables

--- cmd/kube-dns-sync/options.go
+++ cmd/kube-dns-sync/options.go
@@ -18,12 +18,12 @@ import (
 )
 
 var opts struct {
-	DNSProvider       string         `long:"dns-provider" env:"DNS_SYNC_PROVIDER" description:"DNS provider" required:"yes"`
-	DNSProviderConfig flags.Filename `long:"dns-provider-config" env:"DNS_SYNC_PROVIDER_CONFIG" description:"Path to config file for configuring DNS provider"`
-	ZoneName          string         `long:"zone-name" env:"DNS_SYNC_ZONE_NAME" description:"Zone name, like example.com" required:"yes"`
-	SyncInterval      time.Duration  `long:"sync-interval" default:"60s" env:"DNS_SYNC_INTERVAL" description:"Interval for syncing with the DNS Provider"`
-	TTL               int64          `long:"ttl" default:"300" env:"DNS_SYNC_TTL" description:"TTL value of DNS Records"`
-	AddressTypes      addressTypes   `long:"address-types" default:"externalip" env:"DNS_SYNC_ADDRESS_TYPES" description:"Comma list of address types to export [externalip|internalip|legacyhostip]"`
+	DNSProvider       string         `long:"dns-provider" env:"KDS_PROVIDER" description:"DNS provider" required:"yes"`
+	DNSProviderConfig flags.Filename `long:"dns-provider-config" env:"KDS_PROVIDER_CONFIG" description:"Path to config file for configuring DNS provider"`
+	ZoneName          string         `long:"zone-name" env:"KDS_ZONE_NAME" description:"Zone name, like example.com" required:"yes"`
+	SyncInterval      time.Duration  `long:"sync-interval" default:"60s" env:"KDS_INTERVAL" description:"Interval for syncing with the DNS Provider"`
+	TTL               int64          `long:"ttl" default:"300" env:"KDS_TTL" description:"TTL value of DNS Records"`
+	AddressTypes      addressTypes   `long:"address-types" default:"externalip" env:"KDS_ADDRESS_TYPES" description:"Comma list of address types to export [externalip|internalip|legacyhostip]"`
 	Version           func()         `long:"version" short:"v" description:"show version number"`
 }
 
